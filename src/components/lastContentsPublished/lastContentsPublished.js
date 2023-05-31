1fee67294659dbc7fc0479e0b2c278520b3f3078
Guillaume Robert <guillaumerober@gmail.com>
2023-05-31 14:02:33 +0200

    lastContentsPublished component

--- src/components/lastContentsPublished/lastContentsPublished.js
+++ src/components/lastContentsPublished/lastContentsPublished.js
@@ -3,6 +3,13 @@ import reviews from "../../data/reviews.json";
 import { Link } from "react-router-dom";
 import "./lastContentsPublished.css";
 
+function truncateText(text, maxLength) {
+  if (text.length <= maxLength) {
+    return text;
+  }
+  return text.slice(0, maxLength - 3) + "...";
+}
+
 function LastContentsPublished() {
   const articles = data.slice(0, 10); // Sélectionne jusqu'à 10 articles, à déterminer
   const reviewSelect = reviews.slice(0, 4); // Pareil pour les tests, jusqu'à 4 articles
@@ -15,14 +22,18 @@ function LastContentsPublished() {
           {articles.map((a, index) => (
             <Link key={a.id} to={`/article/${a.id}`}>
               <div className="new-article">
-                <div className="new-article-image">
-
-                </div>
+                <div className="new-article-image"></div>
                 <div className="new-article-info">
+                  <Link key={a.id} to={`/article/${a.id}`}>
                     <h3>{a.title}</h3>
-                    <p className="new-article-date">{a.date}</p>
-                    <p className="new-article-description">{a.description}</p>
-                    <p className="new-article-author">{a.author}</p>
+                  </Link>
+                  <p className="new-article-date">{a.date}</p>
+                  <Link key={a.id} to={`/article/${a.id}`}>
+                    <p className="new-article-description">{truncateText(a.description, 200)}</p>
+                  </Link>
+                  <p>
+                    Par <span className="new-article-author">{a.author}</span>
+                  </p>
                 </div>
               </div>
             </Link>
@@ -30,17 +41,20 @@ function LastContentsPublished() {
         </div>
       </div>
       <div className="last-reviews-area">
-       <div className="last-reviews-block">
-            <h2>Les derniers tests</h2>
-            {reviewSelect.map((r, index) => (
+        <div className="last-reviews-block">
+          <h2>Les derniers tests</h2>
+          {reviewSelect.map((r, index) => (
             <Link key={r.id} to={`/test/${r.id}`}>
-                <div className="new-review">
-                    <h3>{r.title}</h3>
-                    <p>{r.author}</p>
-                </div>
+              <div className="new-review">
+                <div className="new-review-info">
+                  <h3>{r.title}</h3>
+                  <p> Par <span className="new-article-author">{r.author}</span></p>
+                </div> 
+                <div className="review-placeholder"></div>
+              </div>
             </Link>
-         ))}
-        </div> 
+          ))}
+        </div>
       </div>
     </>
   );
