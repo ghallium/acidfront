9f8b029e91ae9b462332b18036c474de77a48218
ghallium <guillaumerober@gmail.com>
2023-06-20 16:26:01 +0200

    actu page wip

--- src/pages/actu/actu.js
+++ src/pages/actu/actu.js
@@ -1,7 +1,34 @@
+import ArticlesData from "../../data/articles.json";
+import { Link } from "react-router-dom";
+import "./actu.css";
+
 function Actu() {
-    return(
-        <p>Je suis la page d'actu</p>
-    )
+    return (
+        <div className="actu-pageblock">
+            <h2>Toutes les dernières news</h2>
+            <div className="actu-container">
+                {ArticlesData.filter((adata) => adata["content-type"] === "News").map((adata) => (
+                    <Link key={adata.id} to={`/article/${adata.id}`}>
+                        <div className="article">
+                            <div
+                                className="article-minia"
+                                style={{
+                                    backgroundImage: `url(${adata.miniature})`,
+                                    backgroundPosition: "center",
+                                    backgroundSize: "cover",
+                                    backgroundRepeat: "no-repeat",
+                                }}
+                            ></div>
+                            <div className="article-info">
+                                <div className="article-title">{adata.title}</div>
+                                <div className="article-description">{adata.description}</div>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default Actu;
+export default Actu
