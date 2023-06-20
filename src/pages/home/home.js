ebe1f14f33b0d838dc2d3ff13a3e2d61b63034c5
ghallium <guillaumerober@gmail.com>
2023-06-20 14:09:23 +0200

    home mobile

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,12 +1,57 @@
 import Searchbar from "../../components/searchbar/searchbar.js";
 import data from "../../data/articles.json";
-// import { Link } from "react-router-dom";
 import LastContentsPublished from "../../components/lastContentsPublished/lastContentsPublished.js";
 import "./home.css";
 
 function Home() {
-  const articles = data.slice(0, 7); // Sélectionne les 7 premiers articles
+  const articles = data.slice(0, 7);
   const featuredArticles = articles.filter((a) => a["article-une"]);
+
+  const isWindowWidthBetween = (minWidth, maxWidth) => {
+    return (
+      window.innerWidth >= minWidth && window.innerWidth <= maxWidth
+    );
+  };
+
+  const renderCards = () => {
+    if (isWindowWidthBetween(350, 1024)) {
+      // Rendre seulement deux éléments de la classe "card" lorsque le navigateur est entre 350px et 1024px de large
+      const twoCards = articles.filter((a) => !a["article-une"]).slice(0, 2);
+
+      return twoCards.map((a) => (
+        <div
+          className="card"
+          key={a.title}
+          style={{
+            backgroundImage: `url(${a.card})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="card-title">{a.title.slice(0, 30)}</div>
+        </div>
+      ));
+    }
+
+    // Rendre tous les éléments de la classe "card"
+    return articles.map((a) => (
+      !a["article-une"] && (
+        <div
+          className="card"
+          key={a.title}
+          style={{
+            backgroundImage: `url(${a.card})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="card-title">{a.title.slice(0, 30)}</div>
+        </div>
+      )
+    ));
+  };
 
   return (
     <>
@@ -14,31 +59,21 @@ function Home() {
         <Searchbar />
         <div id="cards-area">
           {featuredArticles.map((a) => (
-                <div className="featured-card" style={{
-                  backgroundImage: `url(${a.card})`,
-                  backgroundPosition: 'center',
-                  backgroundSize: 'cover',
-                  backgroundRepeat: 'no-repeat',
-              }}>
-                  {/* Contenu de la carte en vedette */}
-                  <div className="featured-card-title">{a.title}</div>
-                </div>
+            <div
+              className="featured-card"
+              style={{
+                backgroundImage: `url(${a.card})`,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+              }}
+              key={a.title}
+            >
+              <div className="featured-card-title">{a.title}</div>
+            </div>
           ))}
-          
-          {articles.map((a) => (
-            !a["article-une"] && (                
-                  <div className="card" key={a.title} style={{
-                    backgroundImage: `url(${a.card})`,
-                    backgroundPosition: 'center',
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat',
-                }}>
-                     {/* Contenu de la carte normale, penser à retirer le slice */}
-                  <div className="card-title">{a.title.slice(0,30)}</div>
-                
-                </div>
-            )
-          ))}
+
+          {renderCards()}
         </div>
       </div>
       <div className="bottomBlock">
